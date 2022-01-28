605edbdbe6742db894b59022f2406ae2d057aab1
HTN <96134288+htn-dev@users.noreply.github.com>
2022-01-28 15:06:21 -0500

    Create create-static-website-s3.sh

---
+++ create-static-website-s3.sh
@@ -0,0 +1,9 @@
+# Create an S3 bucket
+aws s3 mb s3://REPLACE_ME_BUCKET_NAME
+
+# Enables the objects in the bucket to be requested using a registered public DNS name for the bucket, 
+# as well as direct site requests to the base path of the DNS name to a selected website homepage (index.html in most cases):
+aws s3 website s3://REPLACE_ME_BUCKET_NAME --index-document index.html
+
+# Add a public bucket policy to your website
+aws s3api put-bucket-policy --bucket REPLACE_ME_BUCKET_NAME --policy file://~/environment/aws-modern-application-workshop/module-1/aws-cli/website-bucket-policy.json
